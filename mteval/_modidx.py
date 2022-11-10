5fafa5da772c19ba97d3cd520d411930b5d1e259
Achim Ruopp <achim@polyglot.technology>
2022-11-10 15:59:29 -0500

    Microsoft Translator module added

--- mteval/_modidx.py
+++ mteval/_modidx.py
@@ -11,4 +11,10 @@ d = { 'settings': { 'branch': 'main',
                                 'mteval.dataset.get_translated_test_set': ('dataset.html#get_translated_test_set', 'mteval/dataset.py'),
                                 'mteval.dataset.read_own_set': ('dataset.html#read_own_set', 'mteval/dataset.py'),
                                 'mteval.dataset.read_source_ref': ('dataset.html#read_source_ref', 'mteval/dataset.py'),
-                                'mteval.dataset.read_tsv_set': ('dataset.html#read_tsv_set', 'mteval/dataset.py')}}}
+                                'mteval.dataset.read_tsv_set': ('dataset.html#read_tsv_set', 'mteval/dataset.py')},
+            'mteval.microsoftmt': { 'mteval.microsoftmt.microsofttranslate': ( 'microsoftmt.html#microsofttranslate',
+                                                                               'mteval/microsoftmt.py'),
+                                    'mteval.microsoftmt.microsofttranslate.__init__': ( 'microsoftmt.html#microsofttranslate.__init__',
+                                                                                        'mteval/microsoftmt.py'),
+                                    'mteval.microsoftmt.microsofttranslate.translate_text': ( 'microsoftmt.html#microsofttranslate.translate_text',
+                                                                                              'mteval/microsoftmt.py')}}}
